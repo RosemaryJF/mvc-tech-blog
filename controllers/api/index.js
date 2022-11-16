edfad44b8543511e544d3c383babac6bbd7cc8d1
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-11-16 11:28:37 +1100

    addition of dashboard route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
-const feedRoutes = require('./feedRoutes');
 const userRoutes = require('./userRoutes');
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/user', userRoutes);
-router.use('/feed', feedRoutes);
+router.use('/blog', blogRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
